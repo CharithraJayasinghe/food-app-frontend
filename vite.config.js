f05dfc5e133c127506465fd9ac681992b7301a9e
“Charithra” <charithra@adtechnologies.lk>
2024-06-13 11:10:23 +0530

    change backend url

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://food-app-backend-api.vercel.app/",
-      "/uploads/": "https://food-app-backend-api.vercel.app/",
+      // "/api/": "https://food-app-backend-api.vercel.app/",
+      // "/uploads/": "https://food-app-backend-api.vercel.app/",
+
+      "/api/": "http://localhost:5000/",
+      "/uploads/": "http://localhost:5000/",
     },
   },
 })
