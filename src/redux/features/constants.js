8437026e5214e07507ae7ec3653c3ae10cabea27
“Charithra” <charithra@adtechnologies.lk>
2024-06-23 10:10:33 +0530

    add base url

--- src/redux/features/constants.js
+++ src/redux/features/constants.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://food-app-backend-api.vercel.app/";
-export const BASE_URL = "";
+export const BASE_URL = "https://food-app-backend-api.vercel.app/";
 export const USERS_URL = "/api/users";
