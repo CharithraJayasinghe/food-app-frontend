ad875e9dec93d0cb0aa3c433a7331e3d8c029b38
“Charithra” <charithra@adtechnologies.lk>
2024-07-17 23:57:35 +0530

    new

--- src/redux/features/constants.js
+++ src/redux/features/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://food-app-backend-api.vercel.app";
+export const BASE_URL = "https://food-app-backend-api.vercel.app/";
 // export const BASE_URL = "https://food-app-backend-api.vercel.app/";
 // export const BASE_URL = process.env.NODE_ENV === 'production' 
 // ? 'https://food-app-backend-api.vercel.app' 
